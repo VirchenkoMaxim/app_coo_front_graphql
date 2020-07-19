f67d4a94cfbcdf399e83b5a4a6607e34a7724c97
Maxym <maks064040@gmail.com>
2020-07-19 07:58:39 +0300

    add host2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 // const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -15,4 +14,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
