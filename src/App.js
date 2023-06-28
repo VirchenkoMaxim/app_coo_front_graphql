4dfc3b95fba3efe8c42530b9574bd66d5f1c4803
mx <maks064040@gmail.com>
2023-06-28 22:54:24 +0300

    test4

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import Stats from './components/Stats';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
-
-
 const client = new ApolloClient({
   uri: 'https://app-coo-back-graphql.herokuapp.com/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
-function App() {
 
+function App() {
   return (
     <ApolloProvider client={client}>
       <Header />
       <Stats />
       <Footer />
     </ApolloProvider>
-
   );
 }
 
